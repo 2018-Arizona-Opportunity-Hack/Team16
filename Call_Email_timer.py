0a5c6bab521a509a80534bea50de13cce4f9b413
David9HMYeh <41458571+David9HMYeh@users.noreply.github.com>
2018-10-21 13:22:48 -0700

    Update Call_Email_timer.py

--- Call_Email_timer.py
+++ Call_Email_timer.py
@@ -1,48 +1,48 @@
 
 #time converter
-def time_converter(time):
-    import datetime
-    from datetime import timezone
-    d=datetime.datetime.strptime( str(time), "%Y-%m-%dT%H:%M:%S" )
-#     d=datetime.datetime.strptime( "2007-03-04T21:08:12", "%Y-%m-%dT%H:%M:%S" )
-    timestamp = d.replace(tzinfo=timezone.utc).timestamp()
-    return timestamp
-
-
-# In[3]:
-
-
-#create csv file
-import csv
-with open('hack.csv', 'w',encoding='utf-8') as csvfile:
-    fieldnames = ['userID','Interest','Availibility_from','Availibility_to']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-
-
-
-#writing records
-def writeRecord(userID,Interest,Availibility_f,Availibility_t):
-    u = str(userID)
-    af = time_converter(str(Availibility_f))
-    at = time_converter(str(Availibility_t))
-    i = str(Interest)
-
-    with open('hack.csv','a',encoding='utf-8') as fd:
-        fieldnames = ['userID','Interest','Availibility_from','Availibility_to']
-        writer = csv.DictWriter(fd, fieldnames=fieldnames)
-
-        writing_dic={'userID':u, 'Interest':i, 'Availibility_from':af, 'Availibility_to':at}
-        writer.writerow(writing_dic)
-
-
-#Write Recodes
-# writeRecord('001','cats',"2007-03-04T21:08:13","2007-03-04T21:09:11")
-# writeRecord('002','dogs',"2007-03-04T21:06:12","2007-03-04T21:07:12")
-# writeRecord('003','cats',"2007-03-04T21:08:12","2007-03-04T21:20:12")
-# writeRecord('004','cats',"2007-03-04T21:08:12","2007-03-04T21:20:12")
+# def time_converter(time):
+#     import datetime
+#     from datetime import timezone
+#     d=datetime.datetime.strptime( str(time), "%Y-%m-%dT%H:%M:%S" )
+# #     d=datetime.datetime.strptime( "2007-03-04T21:08:12", "%Y-%m-%dT%H:%M:%S" )
+#     timestamp = d.replace(tzinfo=timezone.utc).timestamp()
+#     return timestamp
 #
+#
+# # In[3]:
+#
+#
+# #create csv file
+# import csv
+# with open('hack.csv', 'w',encoding='utf-8') as csvfile:
+#     fieldnames = ['userID','Interest','Availibility_from','Availibility_to']
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#
+#     writer.writeheader()
+#
+#
+#
+# #writing records
+# def writeRecord(userID,Interest,Availibility_f,Availibility_t):
+#     u = str(userID)
+#     af = time_converter(str(Availibility_f))
+#     at = time_converter(str(Availibility_t))
+#     i = str(Interest)
+#
+#     with open('hack.csv','a',encoding='utf-8') as fd:
+#         fieldnames = ['userID','Interest','Availibility_from','Availibility_to']
+#         writer = csv.DictWriter(fd, fieldnames=fieldnames)
+#
+#         writing_dic={'userID':u, 'Interest':i, 'Availibility_from':af, 'Availibility_to':at}
+#         writer.writerow(writing_dic)
+#
+#
+# #Write Recodes
+# # writeRecord('001','cats',"2007-03-04T21:08:13","2007-03-04T21:09:11")
+# # writeRecord('002','dogs',"2007-03-04T21:06:12","2007-03-04T21:07:12")
+# # writeRecord('003','cats',"2007-03-04T21:08:12","2007-03-04T21:20:12")
+# # writeRecord('004','cats',"2007-03-04T21:08:12","2007-03-04T21:20:12")
+# #
 def timer(time):
     from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -82,44 +82,44 @@ def timer(time):
 #     return new_df
 
 #filtering CSV
-import pandas as pd
-df=pd.read_csv('hack.csv',encoding='utf-8')
-
-#para= >,<,==,!=
-
-
-def interestSelect(certain_column,para,standard):
-
-    if (type(df.iloc[:,int(certain_column)][0]) is str):
-        if para == "=":
-            new_df=df.loc[(df.iloc[:,int(certain_column)] == str(standard))]
-
-
-#             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
-            return new_df
-        else:
-            new_df=df.loc[(df.iloc[:,int(certain_column)] != str(standard))]
-#             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
-            return new_df
-    else:
-
-        if para == "=":
-            new_df=df.loc[(df.iloc[:,int(certain_column)] == str(standard))]
-#             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
-            return new_df
-        elif para == ">":
-            new_df=df.loc[(df.iloc[:,int(certain_column)]) > float(standard)]
-#             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
-            return new_df
-        elif para == "<":
-            new_df=df.loc[float((df.iloc[:,int(certain_column)]) < float(standard))]
-#             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
-            return new_df
-        elif para == "!=":
-            new_df=df.loc[(df.iloc[:,int(certain_column)] != str(standard))]
-#             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
-            return new_df
-
+# import pandas as pd
+# df=pd.read_csv('hack.csv',encoding='utf-8')
+#
+# #para= >,<,==,!=
+#
+#
+# def interestSelect(certain_column,para,standard):
+#
+#     if (type(df.iloc[:,int(certain_column)][0]) is str):
+#         if para == "=":
+#             new_df=df.loc[(df.iloc[:,int(certain_column)] == str(standard))]
+#
+#
+# #             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
+#             return new_df
+#         else:
+#             new_df=df.loc[(df.iloc[:,int(certain_column)] != str(standard))]
+# #             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
+#             return new_df
+#     else:
+#
+#         if para == "=":
+#             new_df=df.loc[(df.iloc[:,int(certain_column)] == str(standard))]
+# #             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
+#             return new_df
+#         elif para == ">":
+#             new_df=df.loc[(df.iloc[:,int(certain_column)]) > float(standard)]
+# #             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
+#             return new_df
+#         elif para == "<":
+#             new_df=df.loc[float((df.iloc[:,int(certain_column)]) < float(standard))]
+# #             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
+#             return new_df
+#         elif para == "!=":
+#             new_df=df.loc[(df.iloc[:,int(certain_column)] != str(standard))]
+# #             new_df.to_csv('Filtered_hack.csv', encoding='utf-8')
+#             return new_df
+#
 
 # print(interestSelect(1,"=","cats"))
 
@@ -170,16 +170,15 @@ def timer_SMS(days_,body,to_number):
     scheduler.start()
 
 
-# In[349]:
 
-
-#json to dataframe
-import json
-import pandas as pd
-with open('test.json', 'r') as f:
-    data = json.load(f)
-pd = pd.DataFrame.from_dict(data, orient='index')
-# train.reset_index(level=0, inplace=True)
+#
+# #json to dataframe
+# import json
+# import pandas as pd
+# with open('test.json', 'r') as f:
+#     data = json.load(f)
+# pd = pd.DataFrame.from_dict(data, orient='index')
+# # train.reset_index(level=0, inplace=True)
 
 
 # In[350]:
@@ -189,9 +188,9 @@ pd = pd.DataFrame.from_dict(data, orient='index')
 # In[334]:
 
 
-#select
-import random
+
 def select_group(list_,mode,number):
+    import random
 #     list_=
     if mode=="random":
         random.shuffle(list_)
@@ -219,7 +218,8 @@ def find_email(json):
 # print(find_email(json))
 
 
-def SMS1(body,to_number):
+def SMS1(body,to_number0):
+    to_number0=[]
 
     to_number=4802552099
     from twilio.rest import Client
@@ -238,7 +238,8 @@ def SMS1(body,to_number):
 
     # print(message.sid)
 
-def phone1(to_number):
+def phone1(to_number0):
+    to_number0=[]
     to_number=4802552099
     from twilio.rest import Client
     # Your Account Sid and Auth Token from twilio.com/console
@@ -252,33 +253,12 @@ def phone1(to_number):
                             to='+1'+str(to_number),
                             from_="+14803780697"
                         )
-#
-# print(call.sid)
-
-
-# In[10]:
-
-# def SMS2(body,to_number):
-#     from twilio.rest import Client
-#
-#
-#     # Your Account Sid and Auth Token from twilio.com/console
-#     account_sid = "AC3d0e45500fdd987d07b4cd6f594b7ee5"
-#     auth_token = "91a8726ff72cf2708643a513724d2997"
-#     client = Client(account_sid, auth_token)
-#
-#     message = client.messages.create(
-#                                   body='Hello there!',
-#                                   from_='whatsapp:+14803780697',
-#                                   to='whatsapp:+14802552099'
-#                               )
-#
-#     print(message.sid)
 
 
 # In[6]:
 
-def SMS2(body,to_number):
+def SMS2(body,to_number0):
+    to_number0[]
     to_number=4252406185
     from twilio.rest import Client
 
@@ -299,7 +279,8 @@ def SMS2(body,to_number):
 
 # In[248]:
 
-def phone2(to_number):
+def phone2(to_number0):
+    to_number0=[]
     to_number=4252406185
     from twilio.rest import Client
 
@@ -314,5 +295,3 @@ def phone2(to_number):
                             to='+1'+str(to_number),
                             from_="+12155443131"
                         )
-
-print(call.sid)
